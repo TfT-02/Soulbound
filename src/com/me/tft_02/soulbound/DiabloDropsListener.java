f969a3d5692c3814f6ca36ef4feb9c2f2fb54285
TfT_02 <tft_02@hotmail.com>
2013-03-10 14:35:41 +0100

    Needs an EventHandler offcourse

--- src/com/me/tft_02/soulbound/DiabloDropsListener.java
+++ src/com/me/tft_02/soulbound/DiabloDropsListener.java
@@ -1,5 +1,7 @@
 package com.me.tft_02.soulbound;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 
@@ -11,9 +13,15 @@ import com.modcrafting.diablodrops.tier.Tier;
 public class DiabloDropsListener implements Listener {
     DiabloDrops plugin;
 
-    private DropsAPI dropsAPI = new DropsAPI(plugin);
 
+    /**
+     * Monitor EntitySpawnWithItemEvent events.
+     * 
+     * @param event The event to monitor
+     */
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onEntitySpawnWithItem(EntitySpawnWithItemEvent event) {
+        DropsAPI dropsAPI = new DropsAPI(plugin);
         System.out.println("onEntitySpawnWithItem");
         for (ItemStack item : event.getItems()) {
             Tier tier = dropsAPI.getTier(item);
