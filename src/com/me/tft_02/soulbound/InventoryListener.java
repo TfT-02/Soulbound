17acc775e4a1b2ab553221d0693064fa70af465f
TfT_02 <tft_02@hotmail.com>
2013-03-10 16:48:17 +0100

    Added Bind on Equip and Bind on Use

--- src/com/me/tft_02/soulbound/InventoryListener.java
+++ src/com/me/tft_02/soulbound/InventoryListener.java
@@ -7,6 +7,7 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.event.inventory.InventoryType.SlotType;
 import org.bukkit.inventory.ItemStack;
 
 import com.me.tft_02.soulbound.ItemUtils.ItemType;
@@ -16,6 +17,43 @@ public class InventoryListener implements Listener {
 
     public InventoryListener(Soulbound instance) {
         plugin = instance;
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    void onInventoryClick(InventoryClickEvent event) {
+        HumanEntity entity = event.getWhoClicked();
+        ItemStack itemStack = event.getCurrentItem();
+        SlotType slotType = event.getSlotType();
+        InventoryType inventoryType = event.getInventory().getType();
+
+        if (inventoryType == null) {
+            return;
+        }
+
+        ItemType itemType = ItemUtils.getItemType(itemStack);
+
+        if (itemStack == null) {
+            return;
+        }
+
+        if (entity instanceof Player) {
+            Player player = (Player) entity;
+            switch (itemType) {
+                case BIND_ON_EQUIP:
+                    switch (slotType) {
+                        case ARMOR:
+                            ItemUtils.soulbindItem(player, itemStack);
+                            return;
+                        default:
+                            return;
+                    }
+                case BIND_ON_USE:
+                    ItemUtils.soulbindItem(player, itemStack);
+                    return;
+                default:
+                    break;
+            }
+        }
     }
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
@@ -47,9 +85,6 @@ public class InventoryListener implements Listener {
                     if (!ItemUtils.isBindedPlayer(player, itemStack)) {
                         event.setCancelled(true);
                     }
-                    return;
-                case BIND_ON_USE:
-                    ItemUtils.soulbindItem(player, itemStack);
                     return;
                 default:
                     return;
