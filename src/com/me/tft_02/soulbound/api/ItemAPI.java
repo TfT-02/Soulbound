9462b8207fd7906bd5136f0799def494138e24fe
TfT_02 <tft_02@hotmail.com>
2013-03-13 12:45:29 +0100

    Moar API!

--- src/com/me/tft_02/soulbound/api/ItemAPI.java
+++ src/com/me/tft_02/soulbound/api/ItemAPI.java
@@ -9,16 +9,41 @@ public final class ItemAPI {
     private ItemAPI() {}
 
     /**
-     * Soulbind an itemstack to a player.
+     * Check if a Player is binded to an ItemStack.
      * </br>
      * This function is designed for API usage.
      *
-     * @param player The player to bind the item to
-     * @param itemStack The itemstack to bind
-     * @return the soulbound itemstack
+     * @param player The Player to check
+     * @param itemStack The ItemStack to check
+     * @return true or false
+     */
+    public static boolean isSoulbindedPlayer(Player player, ItemStack itemStack) {
+        return ItemUtils.isBindedPlayer(player, itemStack);
+    }
+
+    /**
+     * Soulbind an ItemStack to a Player.
+     * </br>
+     * This function is designed for API usage.
+     *
+     * @param player The Player to bind the item to
+     * @param itemStack The ItemStack to bind
+     * @return the soulbound ItemStack
      */
     public static ItemStack soulbindItem(Player player, ItemStack itemStack) {
         return ItemUtils.soulbindItem(player, itemStack);
+    }
+
+    /**
+     * Check if an itemstack is Soulbound.
+     * </br>
+     * This function is designed for API usage.
+     *
+     * @param itemStack The itemstack to check
+     * @return true or false
+     */
+    public static boolean isSoulbound(ItemStack itemStack) {
+        return ItemUtils.isSoulbound(itemStack);
     }
 
     /**
@@ -31,6 +56,18 @@ public final class ItemAPI {
      */
     public static ItemStack bindOnPickupItem(ItemStack itemStack) {
         return ItemUtils.bopItem(itemStack);
+    }
+
+    /**
+     * Check if an itemstack is Bind on Pickup.
+     * </br>
+     * This function is designed for API usage.
+     *
+     * @param itemStack The itemstack to check
+     * @return true or false
+     */
+    public static boolean isBindOnPickup(ItemStack itemStack) {
+        return ItemUtils.isBindOnPickup(itemStack);
     }
 
     /**
