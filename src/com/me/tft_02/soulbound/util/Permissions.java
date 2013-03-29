8cac5ccfd360ea90e41071ba7e8a1dadef882bc6
TfT_02 <tft_02@hotmail.com>
2013-03-29 19:21:41 +0100

    This is how to properly get a dynamic permission.

--- src/com/me/tft_02/soulbound/util/Permissions.java
+++ src/com/me/tft_02/soulbound/util/Permissions.java
@@ -1,16 +1,20 @@
 package com.me.tft_02.soulbound.util;
 
 import org.bukkit.entity.Player;
+import org.bukkit.permissions.PermissionAttachmentInfo;
 
 public class Permissions {
 
     public static int getSoulbindMaximum(Player player) {
-        String permission = "soulbound.maximum_allowed.";
+        String match = "soulbound.maximum_allowed.";
         int amount = -1;
 
-        if (player.hasPermission(permission)) {
-            amount = Integer.parseInt(permission.split(".")[2]);
+        for (PermissionAttachmentInfo permission : player.getEffectivePermissions()) {
+            if (permission.getPermission().startsWith(match) && permission.getValue()) {
+                amount = Integer.parseInt(permission.getPermission().split("[.]")[2]);
+            }
         }
+
         return amount;
     }
 }
