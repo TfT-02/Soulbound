48a370afb2ff063caa38e0fe8ca93b9fb11b9e95
TfT_02 <tft_02@hotmail.com>
2013-05-19 10:40:15 +0200

    Fixed silly bug causing all items to have infinite durability

--- src/main/java/com/me/tft_02/soulbound/util/DurabilityUtils.java
+++ src/main/java/com/me/tft_02/soulbound/util/DurabilityUtils.java
@@ -7,7 +7,7 @@ import com.me.tft_02.soulbound.Soulbound;
 public class DurabilityUtils {
 
     public static void handleInfiniteDurability(ItemStack itemStack) {
-        if (Soulbound.getInstance().getConfig().getBoolean("Soulbound.Infinite_Durability")) {
+        if (Soulbound.getInstance().getConfig().getBoolean("Soulbound.Infinite_Durability") && ItemUtils.isSoulbound(itemStack)) {
             itemStack.setDurability((short) 0);
             return;
         }
