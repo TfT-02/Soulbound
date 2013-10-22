7f48718a7bd504081ea7aaeac142be8af8734eb2
Richard Harrah <topplethenunnery@gmail.com>
2013-10-21 21:25:22 -0400

    switching to RandomItemGenerationEvent

--- src/main/java/com/me/tft_02/soulbound/hooks/MythicDropsV2Listener.java
+++ src/main/java/com/me/tft_02/soulbound/hooks/MythicDropsV2Listener.java
@@ -5,7 +5,7 @@ import com.me.tft_02.soulbound.config.Config;
 import com.me.tft_02.soulbound.util.ItemUtils;
 import net.nunnerycode.bukkit.mythicdrops.MythicDrops;
 import net.nunnerycode.bukkit.mythicdrops.api.tiers.Tier;
-import net.nunnerycode.bukkit.mythicdrops.events.CreatureEquippedWithItemStackEvent;
+import net.nunnerycode.bukkit.mythicdrops.events.RandomItemGenerationEvent;
 import net.nunnerycode.bukkit.mythicdrops.managers.TierManager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -20,12 +20,12 @@ public class MythicDropsV2Listener implements Listener {
     }
 
     /**
-     * Check EntitySpawnWithItemEvent events.
+     * Check RandomItemGenerationEvent events.
      * 
      * @param event The event to check
      */
-    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    public void onMythicDropsEntitySpawn(CreatureEquippedWithItemStackEvent event) {
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onMythicDropsEntitySpawn(RandomItemGenerationEvent event) {
         ItemStack itemStack = event.getItemStack();
 
         ItemStack newItemStack = handleMythicDropsItems(itemStack.clone());
